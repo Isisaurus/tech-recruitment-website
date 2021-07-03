74b1eadfbda4f547997026197d2f43ae07a3d4c2
Isisaurus <dianavitanyi@gmail.com>
2021-07-03 14:44:51 +0200

    Read data from contentful using getStaticProps.

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
-import { createClient } from 'contentful';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
-
-export async function getStaticProps() {
-  // make connection to contentful space
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  });
-}
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/vacatures/index.jsx
+++ pages/vacatures/index.jsx
@@ -1,3 +1,24 @@
-export default function Vacancies() {
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {
+  // make connection to contentful space
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({
+    content_type: 'job',
+  });
+
+  return {
+    props: {
+      jobs: res.items,
+    },
+  };
+}
+
+export default function Vacancies({ jobs }) {
+  console.log(jobs);
   return <div>Vacancy List</div>;
 }
