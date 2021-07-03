9562a5f7877285c6883411e0f900b7ad2f2af87d
Isisaurus <dianavitanyi@gmail.com>
2021-07-03 15:02:54 +0200

    Image tags added and configured.

--- components/JobCard.jsx
+++ components/JobCard.jsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
+
 export default function JobCard({ job }) {
+  const {
+    jobTitle,
+    slug,
+    company,
+    location,
+    salaryIndication,
+    thumbnail,
+    requirements,
+    benefits,
+    details,
+    motivationLetter,
+    jobType,
+  } = job.fields;
   return (
     <div>
-      <span>{job.fields.jobTitle}</span>
+      <Image
+        src={`https://${thumbnail.fields.file.url}`}
+        width={thumbnail.fields.file.details.image.width}
+        height={thumbnail.fields.file.details.image.height}
+      ></Image>
+      <span>{jobTitle}</span>
     </div>
   );
 }
