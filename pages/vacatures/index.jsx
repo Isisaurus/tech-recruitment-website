5404d34c629e2c9e68142fab32a08fa772dba029
Isisaurus <dianavitanyi@gmail.com>
2021-07-05 20:25:43 +0200

    Filtering with dynamically provided query from component.

--- pages/vacatures/index.jsx
+++ pages/vacatures/index.jsx
@@ -1,31 +1,54 @@
+import { useState, useEffect } from 'react';
 import { createClient } from 'contentful';
 import { ContentfulApi } from '../../utils/contentful';
 import JobCard from '../../components/JobCard';
 import Pagination from '../../components/Pagination';
+import Select from 'react-select';
+
+const fetchJobs = async (query, page) => {
+  const data = await ContentfulApi.callContentful(query, page);
+  return data;
+};
 
 export async function getStaticProps() {
-  const jobs = await ContentfulApi.getPaginatedItemSummaries(1);
-  const totalPages = Math.ceil(jobs.total / process.env.PAGE_SIZE);
+  const data = await fetchJobs();
+  const totalPages = data.total / process.env.PAGE_SIZE;
+
+  const { items, cities } = data;
 
   return {
     props: {
-      jobs: jobs.items,
-      currentPage: 1,
+      jobs: items,
       totalPages,
     },
-    // fixed?
     revalidate: 1,
   };
 }
 
 export default function Vacancies({ jobs, currentPage, totalPages }) {
-  // console.log(jobs);
+  // // console.log(jobs);
   // console.log(currentPage);
   // console.log(totalPages);
+
+  const [data, setData] = useState(jobs);
+  const handleDataChange = async () => {
+    const newData = await fetchJobs({ cities: `Amsterdam,Den Haag` }, 1);
+    setData(newData.items);
+  };
+
+  // console.log(data);
+
+  useEffect(() => {}, [data]);
+
   return (
     <div style={{ marginLeft: '200px' }}>
-      {jobs.map((job) => (
-        <JobCard key={job.sys.id} job={job} />
+      <div>
+        <form>{/* <Select /> */}</form>
+        <button onClick={handleDataChange}>Clcik to query</button>
+      </div>
+      {data.map((job) => (
+        // <JobCard key={job.sys.id} job={job} />
+        <div key={job.sys.id}>job</div>
       ))}
       <Pagination currentPage={currentPage} totalPages={totalPages} />
     </div>
--- utils/contentful.js
+++ utils/contentful.js
@@ -1,154 +1,43 @@
-const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID;
-const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN;
+import { createClient } from 'contentful';
 
-export async function fetchContent(query) {
-  try {
-    const res = await fetch(
-      `https://graphql.contentful.com/content/v1/spaces/${space}`,
-      {
-        method: 'POST', // GraphQL *always* uses POST requests!
-        headers: {
-          'content-type': 'application/json',
-          authorization: `Bearer ${accessToken}`, // add access token header
-        },
-        // send the query in GraphiQL as a string
-        body: JSON.stringify({ query }),
-      }
-    );
-    const { data } = await res.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-export async function createQuery(page = 1) {
-  const pageSize = 6;
-  const total = await fetchContent(`
-  {
-    jobCollection {
-       total
-     }
-   }
-  `);
-  const numOfTotalItems = total.jobCollection.total || 0;
-  const skipMultiplier = page === 1 ? 0 : page - 1;
-  const skip = skipMultiplier > 0 ? pageSize * skipMultiplier : 0;
-
-  console.log(page);
-  console.log(skip);
-  return `
-  {
-    jobCollection (limit: ${pageSize}, skip: ${skip}) {
-              items {
-                sys {
-                  id
-                },
-                jobTitle,
-                company,
-                city,
-                salaryIndication,
-                benefits,
-                requirements,
-                jobType,
-                motivationLetter,
-                thumbnail {
-                  title
-                  description
-                  contentType
-                  fileName
-                  size
-                  url
-                  width
-                  height
-                },
-                slug
-              }
-            }
-    }
-  `;
-}
+const client = createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+});
 
 // create a class out of all functions
 export class ContentfulApi {
+  static async getValues() {}
   // make fetch call to contentful API
-  static async callContentful(query) {
+  static async callContentful(query, page = 1) {
     try {
-      const res = await fetch(
-        `https://graphql.contentful.com/content/v1/spaces/${space}`,
-        {
-          method: 'POST', // GraphQL *always* uses POST requests!
-          headers: {
-            'content-type': 'application/json',
-            authorization: `Bearer ${accessToken}`, // add access token header
-          },
-          // send the query in GraphiQL as a string
-          body: JSON.stringify({ query }),
-        }
-      );
-      const { data } = await res.json();
-      return data;
+      console.log(query);
+      const res = await client.getEntries({
+        content_type: 'job',
+        limit: process.env.PAGE_SIZE,
+        skip: page === 1 ? 0 : page - 1,
+        'fields.city': {
+          // ['in']: 'Nijmegen,Amsterdam,Den Haag',
+          ['in']: query?.cities || undefined,
+        },
+        'fields.salaryIndication': {
+          ['gte']: query?.gte || undefined,
+          ['lte']: query?.lte || undefined,
+        },
+        'fields.jobType': query?.jobType || undefined,
+      });
+
+      let citiesArr = [];
+
+      res.items.forEach((item) => {
+        citiesArr.push(item.fields.city);
+      });
+
+      console.log(citiesArr);
+      // console.log(res);
+      return { total: res.total, items: res.items };
     } catch (err) {
-      console.log(err);
+      console.log(err + '💥💥💥');
     }
-  }
-  // get number of all items from contentful
-  static async getTotalPostsNumber() {
-    const query = `
-      {
-        jobCollection {
-           total
-         }
-       }
-      `;
-
-    const response = await this.callContentful(query);
-    const totalPosts = response.jobCollection.total
-      ? response.jobCollection.total
-      : 0;
-
-    return totalPosts;
-  }
-  //pagination
-  static async getPaginatedItemSummaries(page) {
-    const skipMultiplier = page === 1 ? 0 : page - 1;
-    const skip =
-      skipMultiplier > 0 ? process.env.PAGE_SIZE * skipMultiplier : 0;
-
-    const query = ` {
-        jobCollection (limit: ${process.env.PAGE_SIZE}, skip: ${skip}) {
-                total
-                  items {
-                    sys {
-                      id
-                    },
-                    jobTitle,
-                    company,
-                    city,
-                    salaryIndication,
-                    benefits,
-                    requirements,
-                    jobType,
-                    motivationLetter,
-                    thumbnail {
-                      url
-                      width
-                      height
-                    },
-                    slug
-                  }
-                }
-        }`;
-
-    // Call out to the API
-    const response = await this.callContentful(query);
-    const data = response.jobCollection.items.length
-      ? {
-          total: response.jobCollection.total,
-          items: response.jobCollection.items,
-        }
-      : { total: 0, items: [] };
-
-    return data;
   }
 }
