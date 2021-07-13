23b9026df0a5659397d63d9dbf96e267cf8f74d9
Isisaurus <dianavitanyi@gmail.com>
2021-07-13 18:50:14 +0200

    Contact from added with custom submit button.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,9 +10,16 @@ import {
   MenuItem,
   SwipeableDrawer,
   IconButton,
+  DialogContent,
+  CircularProgress,
+  DialogTitle,
+  DialogActions,
+  Dialog,
+  TextField,
 } from '@material-ui/core';
 
 import MenuOpenOutlinedIcon from '@material-ui/icons/MenuOpenOutlined';
+import DoneIcon from '@material-ui/icons/Done';
 
 import { useState } from 'react';
 
@@ -83,6 +90,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Navbar() {
+  const [icon, setIcon] = useState(null);
+  const [show, setShow] = useState(false);
   const [open, setOpen] = useState(false);
   const toggleDrawer = (e) => {
     if (e && e.type === 'keydown' && (e.key === 'Tab' || e.key === 'Shift')) {
@@ -91,94 +100,172 @@ function Navbar() {
     setOpen(!open);
   };
 
+  const handleClickOpen = () => {
+    setIcon(null);
+    setShow(true);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleMessage = (e) => {
+    e.preventDefault();
+    // style button 1.
+    setIcon(<CircularProgress size="1rem" style={{ color: 'white' }} />);
+    setTimeout(() => {
+      setTimeout(() => {
+        // close windows
+        handleClose();
+        if (open) {
+          toggleDrawer();
+        }
+      }, 2000);
+      // style button 2.
+      setIcon(<DoneIcon />);
+    }, 3000);
+  };
+
   const classes = useStyles();
   return (
-    <AppBar position="static" color="transparent" className={classes.AppBar}>
-      <Toolbar style={{ padding: '0' }}>
-        <Link href="/">
-          <a className={classes.link}>
-            <Typography component="h1" className={classes.logo}>
-              Tech-Recruitment
-            </Typography>
-          </a>
-        </Link>
-        <Hidden xsDown>
-          <div className={classes.linksContainer}>
-            <Link href="/vacatures">
-              <a className={classes.link}>
-                <Typography variant="subtitle2" className={classes.navEl}>
-                  Vacatures
-                </Typography>
-              </a>
-            </Link>
-            <Link href="/over-ons">
-              <a className={classes.link}>
-                <Typography variant="subtitle2" className={classes.navEl}>
-                  Over ons
-                </Typography>
-              </a>
-            </Link>
-            <Button
-              variant="contained"
-              size="small"
-              className={classes.button}
-              color="primary"
-            >
-              <Typography>Contact</Typography>
-            </Button>
-          </div>
-        </Hidden>
-
-        <Hidden implementation="css" smUp>
-          <IconButton
-            onClick={toggleDrawer}
-            color="primary"
-            className={classes.burger}
-          >
-            <MenuOpenOutlinedIcon size="small" />
-          </IconButton>
-          <SwipeableDrawer
-            anchor="right"
-            open={open}
-            onClose={toggleDrawer}
-            onOpen={toggleDrawer}
-            disableBackdropTransition
-          >
-            <div className={classes.drawer}>
-              <Link href="/">
-                <a className={classes.burgerLink}>
-                  <MenuItem onClick={toggleDrawer}>
-                    <Typography variant="subtitle2">Home</Typography>
-                  </MenuItem>
-                </a>
-              </Link>
+    <>
+      <AppBar position="static" color="transparent" className={classes.AppBar}>
+        <Toolbar style={{ padding: '0' }}>
+          <Link href="/">
+            <a className={classes.link}>
+              <Typography component="h1" className={classes.logo}>
+                Tech-Recruitment
+              </Typography>
+            </a>
+          </Link>
+          <Hidden xsDown>
+            <div className={classes.linksContainer}>
               <Link href="/vacatures">
-                <a className={classes.burgerLink}>
-                  <MenuItem onClick={toggleDrawer}>
-                    <Typography variant="subtitle2">Vacatures</Typography>
-                  </MenuItem>
+                <a className={classes.link}>
+                  <Typography variant="subtitle2" className={classes.navEl}>
+                    Vacatures
+                  </Typography>
                 </a>
               </Link>
               <Link href="/over-ons">
-                <a className={classes.burgerLink}>
-                  <MenuItem onClick={toggleDrawer}>
-                    <Typography variant="subtitle2">Over ons</Typography>
-                  </MenuItem>
+                <a className={classes.link}>
+                  <Typography variant="subtitle2" className={classes.navEl}>
+                    Over ons
+                  </Typography>
                 </a>
               </Link>
               <Button
                 variant="contained"
                 size="small"
+                className={classes.button}
                 color="primary"
-                style={{ marginTop: '1rem' }}
+                onClick={handleClickOpen}
               >
                 <Typography>Contact</Typography>
               </Button>
             </div>
-          </SwipeableDrawer>
-        </Hidden>
-      </Toolbar>
-    </AppBar>
+          </Hidden>
+
+          <Hidden implementation="css" smUp>
+            <IconButton
+              onClick={toggleDrawer}
+              color="primary"
+              className={classes.burger}
+            >
+              <MenuOpenOutlinedIcon size="small" />
+            </IconButton>
+            <SwipeableDrawer
+              anchor="right"
+              open={open}
+              onClose={toggleDrawer}
+              onOpen={toggleDrawer}
+              disableBackdropTransition
+            >
+              <div className={classes.drawer}>
+                <Link href="/">
+                  <a className={classes.burgerLink}>
+                    <MenuItem onClick={toggleDrawer}>
+                      <Typography variant="subtitle2">Home</Typography>
+                    </MenuItem>
+                  </a>
+                </Link>
+                <Link href="/vacatures">
+                  <a className={classes.burgerLink}>
+                    <MenuItem onClick={toggleDrawer}>
+                      <Typography variant="subtitle2">Vacatures</Typography>
+                    </MenuItem>
+                  </a>
+                </Link>
+                <Link href="/over-ons">
+                  <a className={classes.burgerLink}>
+                    <MenuItem onClick={toggleDrawer}>
+                      <Typography variant="subtitle2">Over ons</Typography>
+                    </MenuItem>
+                  </a>
+                </Link>
+                <Button
+                  variant="contained"
+                  size="small"
+                  color="primary"
+                  style={{ marginTop: '1rem' }}
+                  onClick={handleClickOpen}
+                >
+                  <Typography>Contact</Typography>
+                </Button>
+              </div>
+            </SwipeableDrawer>
+          </Hidden>
+        </Toolbar>
+      </AppBar>
+      <div>
+        <Dialog
+          open={show}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Contact Us</DialogTitle>
+          <form onSubmit={handleMessage}>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                placeholder="Your email address..."
+                label="Email Address"
+                type="email"
+                fullWidth
+                variant="outlined"
+                required
+              />
+              <TextField
+                id="outlined-multiline-static"
+                label="Your message"
+                multiline
+                rows={6}
+                placeholder="Type your message here..."
+                variant="outlined"
+                style={{ marginTop: '2rem' }}
+                required
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions style={{ marginTop: '1rem' }}>
+              <Button onClick={handleClose} color="primary" variant="text">
+                Cancel
+              </Button>
+              <Button
+                color="primary"
+                variant="contained"
+                endIcon={icon}
+                type="submit"
+              >
+                Send
+              </Button>
+            </DialogActions>
+          </form>
+        </Dialog>
+      </div>
+    </>
   );
 }
 
