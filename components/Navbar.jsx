f68fab91c125c8254dfe21ee2a47b63fae1b4bd1
Isisaurus <dianavitanyi@gmail.com>
2021-07-15 13:55:25 +0200

    Navbar modular stling bug fix attempt 2.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import Link from 'next/link';
-import useStyles from '../styles/Navbar.styles';
+// import useStyles from '../styles/Navbar.styles';
 import {
   Typography,
   AppBar,
@@ -17,12 +17,79 @@ import {
   DialogActions,
   Dialog,
   TextField,
+  makeStyles,
 } from '@material-ui/core';
 
 import MenuOpenOutlinedIcon from '@material-ui/icons/MenuOpenOutlined';
 import DoneIcon from '@material-ui/icons/Done';
 
 import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  AppBar: {
+    boxShadow: 'none',
+    marginTop: '1rem',
+    padding: '0 2rem',
+    [theme.breakpoints.down('xs')]: {
+      padding: '0 .5rem',
+      marginTop: '0',
+    },
+  },
+  logo: {
+    marginRight: '1em',
+    fontSize: '1rem',
+    filter: 'none',
+    background: 'linear-gradient( 90deg , #1E8DFF, #BE6CFF)',
+    fontWeight: 700,
+    WebkitTextFillColor: 'transparent',
+    WebkitBackgroundClip: 'text',
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '1.5rem',
+    },
+  },
+  link: {
+    textDecoration: 'none',
+    color: '#7a7a7a',
+    '&:visited': {
+      color: '#7a7a7a',
+      '&:hover': {
+        color: '#1E8DFF',
+      },
+    },
+    '&:hover': {
+      color: '#7a7a7a',
+    },
+  },
+  burgerLink: {
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+  button: {
+    marginLeft: 'auto',
+    textTransform: 'none',
+    [theme.breakpoints.up('md')]: {
+      padding: '.3em 2.3em',
+    },
+  },
+  navEl: {
+    marginLeft: '1.5em',
+  },
+  linksContainer: {
+    display: 'flex',
+    flex: '1',
+    alignItems: 'center',
+  },
+  drawer: {
+    padding: '2rem',
+    textAlign: 'center',
+  },
+  burger: {
+    position: 'absolute',
+    right: '0',
+    top: '1rem',
+    padding: '0',
+  },
+}));
 
 function Navbar() {
   const router = useRouter();
